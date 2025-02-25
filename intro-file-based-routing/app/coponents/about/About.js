9f497757fbcaf7fb2980eba2e706f46073098680
chhaya03 <0188chhayamishra@gmail.com>
2025-02-26 00:36:27 +0530

    comment

---
+++ intro-file-based-routing/app/coponents/about/About.js
@@ -0,0 +1,13 @@
+//this is file based routing
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      I am about
+    </div>
+  )
+}
+
+export default About
---
+++ intro-file-based-routing/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div>
+
+<h1 className="text-3xl">Problem solved by Next.js</h1>
+<ul>
+  <li>full Stack solution</li>
+  <li>File based Routing</li>
+  <li>Additional features like router from next/navigation</li>
+  <li>Optimized rendering</li>
+</ul>
+  
+      </div>
+  );
+}
