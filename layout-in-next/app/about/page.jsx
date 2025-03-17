71206facb1c0681794909df0b3494b1e1fee6747
chhaya03 <0188chhayamishra@gmail.com>
2025-03-17 21:06:55 +0530

    comment

---
+++ layout-in-next/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      facebook is Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+      nisi perferendis dolores deleniti modi distinctio accusantium saepe
+      similique voluptas reiciendis.
+    </div>
+  );
+};
+
+export default page;
